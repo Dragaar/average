4ff7f6b929002f53150e79e4f3fe34cc9c485bd6
Dragaar <rosivanyshyn@gmail.com>
2022-08-14 15:54:49 +0300

    111

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -1,12 +1,29 @@
 package com.epam.rd.autotasks;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Average {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
+    private static int average(List<Integer> numbers) {
+        return (int) numbers.stream()
+                .mapToInt(s -> s)
+                .average()
+                .orElse(0);
 
+    }
+    public static void main(String[] args) {
+        List <Integer> list = new ArrayList();
+
+        Scanner scanner = new Scanner(System.in);
+        int numb = scanner.nextInt();
+        while (numb != 0)
+        {
+            list.add(numb);
+            numb = scanner.nextInt();
+        }
+        System.out.println(average(list));
     }
 
 }
